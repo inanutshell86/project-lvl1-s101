66529ea034caf9f062287007265fbe7077425e62
Ihor Khrystiuk <inanutshell86@gmail.com>
2017-06-08 17:21:11 +0300

    add a logic fot the brain-calc game

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import readlineSync from 'readline-sync';
+import randomstring from 'randomstring';
 
 export const showBrainGames = () => {
   console.log('Welcome to the Brain Games!\n');
@@ -26,6 +27,42 @@ export const showBrainEven = () => {
       count += 1;
     } else if (isEven(gameQuestion) !== userAnswer) {
       console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${isEven(gameQuestion)}".\nLet's try again, ${userName}!`);
+    }
+  }
+  if (count === 3) {
+    console.log(`Congratulations, ${userName}`);
+  }
+};
+export const showBrainCalc = () => {
+  console.log('Welcome to the Brain Games!\nWhat is the result of the expression?\n');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!\n`);
+  let count = 0;
+  let correctAnswer = 0;
+  for (let i = 0; i < 3; i += 1) {
+    const firstArgument = Math.floor(Math.random() * ((100 - 0) + 1));
+    const makeSign = randomstring.generate({
+      length: 1,
+      charset: '+-*',
+    });
+    const secondArgument = Math.floor(Math.random() * ((100 - 0) + 1));
+    const addArguments = (a, b) => a + b;
+    const subArguments = (a, b) => a - b;
+    const multiArguments = (a, b) => a * b;
+    console.log(`Question: ${firstArgument} ${makeSign} ${secondArgument}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (makeSign === '+') {
+      correctAnswer = addArguments(firstArgument, secondArgument);
+    } else if (makeSign === '-') {
+      correctAnswer = subArguments(firstArgument, secondArgument);
+    } else if (makeSign === '*') {
+      correctAnswer = multiArguments(firstArgument, secondArgument);
+    }
+    if (correctAnswer === Number(userAnswer)) {
+      console.log('Correct!');
+      count += 1;
+    } else if (correctAnswer !== userAnswer) {
+      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".\nLet's try again, ${userName}!`);
     }
   }
   if (count === 3) {
