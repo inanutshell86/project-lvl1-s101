c632c114dc1db0d4ad5694bed8945154c388a919
Ihor Khrystiuk <inanutshell86@gmail.com>
2017-06-09 20:32:40 +0300

    rewrite module according to logic in the game-engine

--- src/games/calculator.js
+++ src/games/calculator.js
@@ -1,37 +1,31 @@
-import readlineSync from 'readline-sync';
 import { makeNumber, makeSign } from '..';
+import startGame from '../game-engine';
 
-const showBrainCalc = () => {
-  console.log('Welcome to the Brain Games!\nWhat is the result of the expression?\n');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!\n`);
-  let count = 0;
-  let correctAnswer = 0;
-  for (let i = 0; i < 3; i += 1) {
-    const firstArgument = makeNumber();
-    const operand = makeSign();
-    const secondArgument = makeNumber();
-    const addArguments = (a, b) => a + b;
-    const subArguments = (a, b) => a - b;
-    const multiArguments = (a, b) => a * b;
-    console.log(`Question: ${firstArgument} ${operand} ${secondArgument}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-    if (operand === '+') {
-      correctAnswer = addArguments(firstArgument, secondArgument);
-    } else if (operand === '-') {
-      correctAnswer = subArguments(firstArgument, secondArgument);
-    } else if (operand === '*') {
-      correctAnswer = multiArguments(firstArgument, secondArgument);
-    }
-    if (correctAnswer === Number(userAnswer)) {
-      console.log('Correct!');
-      count += 1;
-    } else if (correctAnswer !== userAnswer) {
-      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".\nLet's try again, ${userName}!`);
-    }
-  }
-  if (count === 3) {
-    console.log(`Congratulations, ${userName}`);
+const desc = 'What is the result of the expression?';
+
+const min = 1;
+const max = 100;
+
+const calc = (a, b, op) => {
+  switch (op) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '*':
+      return a * b;
+    default:
+      return NaN;
   }
 };
-export default showBrainCalc;
+
+const getGame = () => {
+  const firstArgument = makeNumber(min, max);
+  const secondArgument = makeNumber(min, max);
+  const operand = makeSign();
+  const question = `${firstArgument} ${operand} ${secondArgument}`;
+  const solution = String(calc(firstArgument, secondArgument, operand));
+  return { question, solution };
+};
+
+export default () => startGame(desc, getGame);
