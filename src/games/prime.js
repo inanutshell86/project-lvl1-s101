70eda232246c7ea9fbf1a1d87cef5400f384b1cf
Ihor Khrystiuk <inanutshell86@gmail.com>
2017-06-11 18:12:45 +0300

    replace the desc var name with description

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,7 +1,7 @@
 import startGame from '../game-engine';
 import makeNumber from '../make-number';
 
-const desc = 'Answer "yes" if number prime otherwise answer "no"';
+const description = 'Answer "yes" if number prime otherwise answer "no"';
 
 const min = 1;
 const max = 100;
@@ -25,4 +25,4 @@ const getGame = () => {
   return { question, solution };
 };
 
-export default () => startGame(desc, getGame);
+export default () => startGame(description, getGame);
